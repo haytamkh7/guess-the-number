5cb1da41be814560770f9b16697201b2be76b779
haytamkh7 <66912817+haytamkh7@users.noreply.github.com>
2023-07-31 13:57:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,8 @@ window.onload = function() {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    function generateRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
-
     function generateNumberAndColor() {
         var randomNumber = generateRandomNumber(min, max);
-        var randomColor = generateRandomColor();
-
-        document.body.style.backgroundColor = randomColor;
         document.getElementById('random-number').innerText = randomNumber;
     }
 
